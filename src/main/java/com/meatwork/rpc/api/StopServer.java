3b4e194dd5e2b2e5ccdf219ac53225af5a61e7bb
dbarb <d.barbarin@hotmail.fr>
2024-05-05 13:50:55 +0200

    Add log

--- src/main/java/com/meatwork/rpc/api/StopServer.java
+++ src/main/java/com/meatwork/rpc/api/StopServer.java
@@ -1,6 +1,8 @@
 package com.meatwork.rpc.api;
 
 import com.meatwork.core.api.di.Service;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /*
  * Copyright (c) 2016 Taliro.
@@ -10,8 +12,13 @@ import com.meatwork.core.api.di.Service;
 @RpcService(entrypoint = "serverStop")
 public class StopServer implements Remote<Void>{
 
+	public static final Logger LOGGER = LoggerFactory.getLogger(StopServer.class);
+
 	@Override
 	public HttpResponse execute(Void unused) {
+		if (LOGGER.isDebugEnabled()) {
+			LOGGER.debug("prepare stop server...");
+		}
 		RpcOnStartup.group.shutdownGracefully();
 		return HttpResponse.ok();
 	}
